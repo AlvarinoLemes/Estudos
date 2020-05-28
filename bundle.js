a57b2b74ce3ae3dd6181255d0f6089308369cff5
AlvarinoLemes <alvarinoguilherme@hotmail.com>
2020-05-28 20:21:02 -0300

    att

---
+++ bundle.js
@@ -0,0 +1,31 @@
+class List {
+  constructor() {
+    this.todos = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = 'Alvarino';
+  }
+
+  mostrarUsuario() {
+    console.log(this.usuario);
+  }
+
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaLista.add('Novo todo');
+};
+
+MinhaLista.mostrarUsuario();
--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-class TodoList {
+class List {
     constructor() {
         this.todos = [];
     }
 
-    addTodo() {
-          this.todos.push('Novo todo');
-          console.log(this.todos);  
+    add(data) {
+          this.data.push(data);
+          console.log(this.data);  
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Alvarino';
+    }
+
+    mostrarUsuario() {
+        console.log(this.usuario);
     }
 }
 
 const MinhaLista = new TodoList();
 
-document.getElementById('novotodo').onclick = function
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostrarUsuario();
